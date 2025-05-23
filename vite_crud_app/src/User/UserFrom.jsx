bb0dc7647da2f959619421782ace2b9a8d7a7185
Neelam <kumarineelam541@gmail.com>
2025-05-24 02:54:37 +0530

    code commit form

---
+++ vite_crud_app/src/User/UserFrom.jsx
@@ -0,0 +1,22 @@
+const Form = () => {
+  return (
+    <div>
+      <h1>User Form</h1>
+      <form>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input type="email" name="email" />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default Form;
---
+++ vite_crud_app/src/User/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import UserForm from './UserForm';
+
+const User = () => {
+  return (
+    <>
+      <h1>USER</h1>
+      <h2>USER FORM</h2>
+      <UserForm />
+
+    </>
+  );
+};
+
+export default User;
