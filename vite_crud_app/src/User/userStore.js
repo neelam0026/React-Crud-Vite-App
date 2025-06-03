d6a3efdcb2ba74d00a2b328a690a2bda317de670
Neelam Kumari <neelamkumari0026@gmail.com>
2025-06-04 05:29:16 +0530

    Update userStore.js
    remove store

--- vite_crud_app/src/User/userStore.js
+++ vite_crud_app/src/User/userStore.js
@@ -1,14 +1,1 @@
-const userStore = {
-  users: [],
-  addUser(user) {
-    this.users.push(user);
-  },
-  removeUser(userId) {
-    this.users = this.users.filter(user => user.id !== userId);
-  },
-  getUsers() {
-    return this.users;
-  },
-};
 
-export default userStore;
